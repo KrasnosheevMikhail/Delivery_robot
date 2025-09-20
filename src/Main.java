46306f37d97e44e2d53ba25981646410ab61486e
Krasnosheev Mikhail <Mkrasn@yandex.ru>
2025-09-20 23:51:55 +0300

    freqlog created

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,56 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
+import java.util.*;
 
 public class Main {
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
 
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-        for (int i = 0; i < 1000; i++) {
-            executorService.submit(new Thread(() -> {
-                String text = generateRoute("RLRFR", 100);
-                int count = 0;
-                for (int j = 0; j < text.length(); j++) {
-                    if (text.charAt(j) == 'R') count++;
+        Thread checkMax = new Thread(() -> {
+            int max = 0;
+            int keyMax = 0;
+            while (!Thread.interrupted()) {
+                synchronized (sizeToFreq) {
+                    try {
+                        sizeToFreq.wait();
+                    } catch (InterruptedException e) {
+                        return;
+                    }
+                    for (int cnt : sizeToFreq.keySet())
+                        if (sizeToFreq.get(cnt) > max) {
+                            max = sizeToFreq.get(cnt);
+                            keyMax = cnt;
+                        }
+                    System.out.println("Текущий лидер среди частот: " + keyMax + " - " + max);
                 }
+            }
+        });
+        checkMax.start();
+        List<Thread> threadList = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            threadList.add(new Thread(() -> {
+                String str = generateRoute("RLRFR", 100);
+                int count = 0;
+                for (int j = 0; j < str.length(); j++) {
+                    if (str.charAt(j) == 'R') count++;
+                }
+                System.out.println("Число поворотов направо: " + count);
                 synchronized (sizeToFreq) {
                     if (!sizeToFreq.containsKey(count)) sizeToFreq.put(count, 1);
                     else sizeToFreq.put(count, sizeToFreq.get(count) + 1);
+                    sizeToFreq.notify();
                 }
-
             }));
-            while (true) {
-                try {
-                    if (!executorService.awaitTermination(1, TimeUnit.MILLISECONDS)) break;
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-
-        executorService.shutdown();
-        int max = 0;
-        int keyMax = 0;
-        for (Integer cnt : sizeToFreq.keySet()) {
-            if (sizeToFreq.get(cnt) > max) {
-                max = sizeToFreq.get(cnt);
-                keyMax = cnt;
-            }
-        }
-        System.out.println("Самое частое количество повторений " + keyMax + " (встретилось " + max + " раз)");
-        System.out.println("Другие размеры:");
-        for (Integer cnt : sizeToFreq.keySet()) {
-            if (!(sizeToFreq.get(cnt) == max))
-                System.out.println("- " + cnt + " ( " + sizeToFreq.get(cnt) + " раз)");
 
         }
+        for (Thread thread : threadList) thread.start();
+        for (Thread thread : threadList) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        while (checkMax.isAlive()) checkMax.interrupt();
+
     }
 
     public static String generateRoute(String letters, int length) {
